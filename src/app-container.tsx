47525d72329dfbbc7a61c30eda15bf1cbee5616d
TrongThuy <thuyhoang.hvtt@gmail.com>
2020-09-05 10:06:24 +0700

    :building_construction: fit: update layout and app structure

--- src/app-container.tsx
+++ src/app-container.tsx
@@ -2,59 +2,66 @@ import React from 'react'
 
 import { ReactQueryDevtools } from 'react-query-devtools'
 import {
-  Provider,
-  useDispatch,
+	Provider,
+	useDispatch,
 } from 'react-redux'
 import styled from 'styled-components'
 
 import { AppRouter } from './app-router'
-import { MasterLayout } from './components/layouts/master-layout'
 import { store } from './redux'
 import {
-  createNewWalletWithPasscode,
-  loadWalletWebAssembly,
-  useWalletRedux,
+	createNewWalletWithPasscode,
+	loadWalletWebAssembly,
+	useWalletRedux,
 } from './redux/wallet'
+import { useGetListCoinToken } from './services/get-list-coin-tokens'
 
 const AppRenderContainer = styled.div`
 
 `
 const AppRender = () => {
-  const dispatch = useDispatch()
-  React.useEffect(() => {
-    dispatch(loadWalletWebAssembly())
-  }, [dispatch])
+	const dispatch = useDispatch()
+	React.useEffect(() => {
+		dispatch(loadWalletWebAssembly())
+	}, [dispatch])
+  
+	// const { status, data, error, isFetching } = useGetListCoinToken()
+	const isLoading = useWalletRedux(s => s.loading)
+  
 
-  const isLoading = useWalletRedux(s => s.loading)
+	React.useEffect(() => {
+		!isLoading && dispatch(createNewWalletWithPasscode({ name: 'khoa', passcode: '122' }))
+	}, [isLoading])
 
 
-  React.useEffect(() => {
-    !isLoading && dispatch(createNewWalletWithPasscode({ name: 'khoa', passcode: '122' }))
-  }, [isLoading])
+  
+	if (isLoading) {
+		return <div>Loading Chain Binary...</div>
+	}
+  
+	// if (isFetching) {
+	// 	return <AppRenderContainer>IsLoading...</AppRenderContainer>
+	// }
+	// if (error) {
+	// 	return <AppRenderContainer>{JSON.stringify(error)}</AppRenderContainer>
+	// }
 
-
-  if (isLoading) {
-    return <div>Loading Chain Binary...</div>
-  }
-
-  return (
-    <AppRenderContainer>
-      render appss
-    </AppRenderContainer>
-  )
+	return (
+		<AppRenderContainer>
+			{/* {status} + {JSON.stringify(data)} */}
+		</AppRenderContainer>
+	)
 }
 
 export const AppContainer = () => {
-  return (
-    <Provider store={store}>
-      <AppRouter>
-        <MasterLayout>
-          <AppRender/>
-        </MasterLayout>
-      </AppRouter>
-      <ReactQueryDevtools initialIsOpen />
-    </Provider>
-  )
+	return (
+		<Provider store={store}>
+			<AppRouter>
+				<AppRender/>
+			</AppRouter>
+			<ReactQueryDevtools initialIsOpen />
+		</Provider>
+	)
 }
 
 
--- src/app-router.tsx
+++ src/app-router.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
+	BrowserRouter as Router,
+	Route,
+	Switch,
 } from 'react-router-dom'
 
 import { HomePage } from './pages/home/home-page'
 
 export const AppRouter = ({ children }) => {
-  return (
-    <Router>
-      <div>
-        <div>{ children }</div>
-        <Switch>
-          <Route path="/about">
-            <HomePage />
-          </Route>
-          <Route path="/users">
-            <HomePage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<>
+				<Switch>
+					<Route path="/about">
+						<HomePage />
+					</Route>
+					<Route path="/users">
+						<HomePage />
+					</Route>
+					<Route exact path="/">
+						<HomePage />
+					</Route>
+				</Switch>
+				<>{ children }</>
+			</>
+		</Router>
+	)
 }
