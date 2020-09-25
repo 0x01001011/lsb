91d3b0d7918443ae20363f0a0f027a4455ed9824
hovonako <khoahoang@kms-technology.com>
2020-09-25 21:42:48 +0700

    Added COSR and temp hide connect wallet

--- src/app-container.tsx
+++ src/app-container.tsx
@@ -8,22 +8,21 @@ import { REACT_REQUEST_CONFIG } from 'constants/api'
 import { PersistGate } from 'redux-persist/integration/react'
 import { AppRouter } from './app-router'
 import { store } from './stores'
-import { loadWalletFromSessionIfExisted, loadWalletWebAssembly, useWalletState } from './stores/implements/wallet'
 
 const AppRenderContainer = styled.div``
 
 const AppRender = ({ children }) => {
-	const dispatch = useDispatch()
-	const isSDKLoaded = useWalletState((s) => s.sdkLoaded)
-	React.useEffect(() => {
-		dispatch(loadWalletWebAssembly())
-	}, [dispatch])
+	// const dispatch = useDispatch()
+	// const isSDKLoaded = useWalletState((s) => s.sdkLoaded)
+	// React.useEffect(() => {
+	// 	dispatch(loadWalletWebAssembly())
+	// }, [dispatch])
 
-	React.useEffect(() => {
-		if (isSDKLoaded) {
-			dispatch(loadWalletFromSessionIfExisted())
-		}
-	}, [isSDKLoaded])
+	// React.useEffect(() => {
+	// 	if (isSDKLoaded) {
+	// 		dispatch(loadWalletFromSessionIfExisted())
+	// 	}
+	// }, [isSDKLoaded])
 
 	return <AppRenderContainer>{children}</AppRenderContainer>
 }
